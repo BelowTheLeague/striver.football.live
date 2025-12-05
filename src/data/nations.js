6e5f07f59f0b5b23b8830375de898671c9d296e2
BelowTheLeague <jack@belowtheleague.com>
2025-12-05 14:55:39 +0000

    Update nations.js

--- src/data/nations.js
+++ src/data/nations.js
@@ -5,7 +5,7 @@ export const nations = [
     group: "Group A",
     flagUrl: "https://flagcdn.com/w320/ma.png",
     shortBio:
-      "Hosts Morocco head into AFCON 2025 with huge expectation and a squad packed with European experience.",
+      "Host nation Morocco arrive with huge expectation and a squad packed with European experience."
   },
   {
     id: "senegal",
@@ -13,23 +13,38 @@ export const nations = [
     group: "Group D",
     flagUrl: "https://flagcdn.com/w320/sn.png",
     shortBio:
-      "Reigning champions and tournament heavyweights, built around a powerful, athletic core.",
+      "Reigning champions and tournament heavyweights built around a powerful, athletic core."
   },
   {
     id: "nigeria",
     name: "Nigeria",
-    group: "Group C",
+    group: "Group D",
     flagUrl: "https://flagcdn.com/w320/ng.png",
     shortBio:
-      "Super Eagles packed with attacking talent and Premier League stars.",
+      "The Super Eagles bring serious attacking firepower and a huge global following."
   },
   {
     id: "egypt",
     name: "Egypt",
-    group: "Group B",
+    group: "Group A",
     flagUrl: "https://flagcdn.com/w320/eg.png",
     shortBio:
-      "Record AFCON winners led by world-class forwards and a huge global following.",
+      "Record AFCON winners with a history of delivering on the biggest continental stage."
   },
-  // add more nations as needed
+  {
+    id: "mali",
+    name: "Mali",
+    group: "Group A",
+    flagUrl: "https://flagcdn.com/w320/ml.png",
+    shortBio:
+      "A talented, technical side always capable of going deep into the knockout rounds."
+  },
+  {
+    id: "zambia",
+    name: "Zambia",
+    group: "Group A",
+    flagUrl: "https://flagcdn.com/w320/zm.png",
+    shortBio:
+      "Former champions with a proud history and an exciting new generation emerging."
+  }
 ];
