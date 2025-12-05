bc512d9d23af6a2f3ce77e10c749e48eaf713a7f
BelowTheLeague <jack@belowtheleague.com>
2025-12-05 14:49:23 +0000

    Add Nations and Nation routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home.jsx";
 import Match from "./pages/Match.jsx";
 import Articles from "./pages/Articles.jsx";
 import Article from "./pages/Article.jsx";
+import Nations from "./pages/Nations.jsx";
+import Nation from "./pages/Nation.jsx";
+
 
 export default function App() {
   return (
@@ -12,27 +15,34 @@ export default function App() {
           <span className="badge">AFCON</span> Live Football Centre
         </div>
         <nav className="nav">
-          <NavLink to="/" className="nav-link">
-            Live Centre
-          </NavLink>
-          <NavLink to="/matches" className="nav-link">
-            Matches
-          </NavLink>
-          <NavLink to="/articles" className="nav-link">
-            Articles
-          </NavLink>
-        </nav>
+<nav className="nav">
+  <NavLink to="/" className="nav-link">
+    Live Centre
+  </NavLink>
+  <NavLink to="/matches" className="nav-link">
+    Matches
+  </NavLink>
+  <NavLink to="/articles" className="nav-link">
+    Articles
+  </NavLink>
+  <NavLink to="/nations" className="nav-link">
+    Nations
+  </NavLink>
+</nav>
+
       </header>
 
       <main className="main">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/matches" element={<Home showOnlyMatches />} />
-          <Route path="/match/:id" element={<Match />} />
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/articles/:id" element={<Article />} />
-          <Route path="*" element={<div>Page not found.</div>} />
-        </Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/matches" element={<Home showOnlyMatches />} />
+  <Route path="/match/:id" element={<Match />} />
+  <Route path="/articles" element={<Articles />} />
+  <Route path="/articles/:id" element={<Article />} />
+  <Route path="/nations" element={<Nations />} />
+  <Route path="/nation/:id" element={<Nation />} />
+  <Route path="*" element={<div>Page not found.</div>} />
+</Routes>
       </main>
 
       <footer className="site-footer">
