f1892ecf7b3854373e1c62366ba57aa1eaa0b86b
BelowTheLeague <jack@belowtheleague.com>
2025-12-05 15:18:58 +0000

    Create authors.js

---
+++ src/data/authors.js
@@ -0,0 +1,24 @@
+export const authors = [
+  {
+    id: "jack-shaw",
+    name: "Jack Shaw",
+    bio: "Creative Director and non-league obsessive bringing AFCON stories to life.",
+    handles: {
+      striver: "@JackOnStriver",
+      x: "@JackOnX",
+      linkedin: "https://www.linkedin.com/in/jackshaw/",
+      instagram: "@jackoninsta",
+      tiktok: "@jackontiktok",
+      mudruck: "@jackonmudruck"
+    }
+  },
+  {
+    id: "guest-reporter",
+    name: "Guest Reporter",
+    bio: "Accredited AFCON match reporter.",
+    handles: {
+      striver: "@StriverReporter",
+      x: "@ReporterOnX"
+    }
+  }
+];
